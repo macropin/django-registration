4a6ecb50afd1a2b0bbdf45f7abe8050b19e72aa1
ubernostrum <devnull@localhost>
2007-02-10 23:20:05 +0000

    Typo in previous

--- urls.py
+++ urls.py
@@ -12,7 +12,7 @@ from django.contrib.auth.views import login, logout
 from views import activate, register
 
 urlpatterns = patterns('',
-                       (r'^activate/(?P<activation_key>[a-fA-f0-9]+)/$', activate),
+                       (r'^activate/(?P<activation_key>[a-fA-F0-9]+)/$', activate),
                        (r'^login/$', login, {'template_name': 'registration/login.html'}),
                        (r'^logout/$', logout, {'template_name': 'registration/logout.html'}),
                        (r'^register/$', register),
