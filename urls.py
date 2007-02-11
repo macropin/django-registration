1e6d905876303e758d9b4b6cab34d4a05c1f67e3
ubernostrum <devnull@localhost>
2007-02-11 01:44:58 +0000

    Flip the activation URLConf back to match on \w+ and add a comment explaining why.

--- urls.py
+++ urls.py
@@ -13,7 +13,11 @@ from django.contrib.auth.views import login, logout
 from views import activate, register
 
 urlpatterns = patterns('',
-                       (r'^activate/(?P<activation_key>[a-fA-F0-9]+)/$', activate),
+                       # Activation keys get matched by \w+ instead of the more specific
+                       # [a-fA-F0-9]+ because a bad activation key should still get to the view;
+                       # that way it can return a sensible "invalid key" message instead of a
+                       # confusing 404.
+                       (r'^activate/(?P<activation_key>\w+)/$', activate),
                        (r'^login/$', login, {'template_name': 'registration/login.html'}),
                        (r'^logout/$', logout, {'template_name': 'registration/logout.html'}),
                        (r'^register/$', register),
