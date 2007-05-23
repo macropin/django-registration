9621cf6654ed984fa730c100a9dab1e7e0993447
ubernostrum <devnull@localhost>
2007-05-23 06:50:34 +0000

    Final bits of packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='registration',
+      version='0.1',
+      description='User-registration application for Django',
+      author='James Bennett',
+      author_email='james@b-list.org',
+      url='http://code.google.com/p/django-registration/',
+      packages=['registration'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
