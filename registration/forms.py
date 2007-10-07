994edb5d1ea8663dd0491a7c95cd7eff15704afb
ubernostrum <devnull@localhost>
2007-10-07 04:41:55 +0000

    Fixed #45: Only do the password-matching check if the password fields otherwise pass validation.

--- registration/forms.py
+++ registration/forms.py
@@ -63,9 +63,10 @@ class RegistrationForm(forms.Form):
         Validates that the two password inputs match.
         
         """
-        if self.cleaned_data['password1'] == self.cleaned_data['password2']:
-            return self.cleaned_data['password2']
-        raise forms.ValidationError(_(u'You must type the same password each time'))
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] == self.cleaned_data['password2']:
+                return self.cleaned_data['password2']
+            raise forms.ValidationError(_(u'You must type the same password each time'))
     
     def save(self, profile_callback=None):
         """
