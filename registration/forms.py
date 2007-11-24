b69b85160afab575f0eacb59a3a5b1863cded71c
ubernostrum <devnull@localhost>
2007-11-24 04:48:17 +0000

    And do the password validation in clean()

--- registration/forms.py
+++ registration/forms.py
@@ -57,15 +57,12 @@ class RegistrationForm(forms.Form):
         except User.DoesNotExist:
             return self.cleaned_data['username']
         raise forms.ValidationError(_(u'This username is already taken. Please choose another.'))
-    
-    def clean_password2(self):
-        """
-        Validates that the two password inputs match.
-        
-        """
-        if self.cleaned_data['password1'] == self.cleaned_data['password2']:
-            return self.cleaned_data['password2']
-        raise forms.ValidationError(_(u'You must type the same password each time'))
+
+    def clean(self):
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError(_(u'You must type the same password each time'))
+        return self.cleaned_data
     
     def save(self, profile_callback=None):
         """
