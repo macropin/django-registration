10f0c6263b9a351b07854ce41175f02edbd5caeb
ubernostrum <devnull@localhost>
2007-12-08 20:36:22 +0000

    Fix deprecated maxlength, add initial Greek and Russian translations

--- registration/models.py
+++ registration/models.py
@@ -186,7 +186,7 @@ class RegistrationProfile(models.Model):
     
     """
     user = models.ForeignKey(User, unique=True, verbose_name=_('user'))
-    activation_key = models.CharField(_('activation key'), maxlength=40)
+    activation_key = models.CharField(_('activation key'), max_length=40)
     
     objects = RegistrationManager()
     
