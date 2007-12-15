00cdb607ab5c899566912bf126c4840cd7ff5e20
ubernostrum <devnull@localhost>
2007-12-15 03:58:54 +0000

    Better example regex in comment

--- registration/urls.py
+++ registration/urls.py
@@ -17,7 +17,7 @@ from registration.views import activate, register
 
 urlpatterns = patterns('',
                        # Activation keys get matched by \w+ instead of the more specific
-                       # [a-fA-F0-9]+ because a bad activation key should still get to the view;
+                       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
                        # that way it can return a sensible "invalid key" message instead of a
                        # confusing 404.
                        url(r'^activate/(?P<activation_key>\w+)/$',
