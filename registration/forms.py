c97d28bfc3c582493392212b1d6d790e53dea1a6
ubernostrum <devnull@localhost>
2008-01-19 04:50:59 +0000

    Docstring for RegistrationForm.clean()

--- registration/forms.py
+++ registration/forms.py
@@ -59,6 +59,13 @@ class RegistrationForm(forms.Form):
         raise forms.ValidationError(_(u'This username is already taken. Please choose another.'))
 
     def clean(self):
+        """
+        Verifies that the values entered into the two password fields
+        match. Note that an error here will end up in
+        ``non_field_errors()`` because it doesn't apply to a single
+        field.
+        
+        """
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 raise forms.ValidationError(_(u'You must type the same password each time'))
