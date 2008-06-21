fa87f6d03a99fb87698dc2bbb47c50fd2664f5c4
ubernostrum <devnull@localhost>
2008-06-21 07:18:56 +0000

    Some PEP 8 cleanup

--- registration/urls.py
+++ registration/urls.py
@@ -12,7 +12,8 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 from django.contrib.auth import views as auth_views
 
-from registration.views import activate, register
+from registration.views import activate
+from registration.views import register
 
 
 urlpatterns = patterns('',
