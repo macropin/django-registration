37fc47268c78e32a2cc67ae794ed726cb43e85e2
James Bennett <james@b-list.org>
2008-09-22 08:54:12 -0500

    Flip a couple imports to appease my inner style nerd

--- registration/management/commands/cleanupregistration.py
+++ registration/management/commands/cleanupregistration.py
@@ -7,8 +7,8 @@ contains the actual logic for determining which accounts are deleted.
 
 """
 
-from django.core.management.base import NoArgsCommand
 from django.core.management.base import CommandError
+from django.core.management.base import NoArgsCommand
 
 from registration.models import RegistrationProfile
 
