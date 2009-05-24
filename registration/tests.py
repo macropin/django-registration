d01535fa50e55d54f7e3901cfe86f64f67bfea93
James Bennett <james@b-list.org>
2009-05-24 00:26:13 -0500

    Start testing activation.

--- registration/tests.py
+++ registration/tests.py
@@ -6,6 +6,8 @@ Unit tests for django-registration.
 from django.core import mail
 from django.test import TestCase
 
+from registration.models import RegistrationProfile
+
 
 class DefaultBackendTestCase(TestCase):
     """
@@ -33,3 +35,17 @@ class DefaultBackendTestCase(TestCase):
         self.assertEqual(new_user.email, 'bob@example.com')
         self.failIf(new_user.is_active)
         self.assertEqual(len(mail.outbox), 1)
+
+    def test_activation(self):
+        """
+        Test the activation process: activating within the permitted
+        window sets the account's ``is_active`` field to ``True`` and
+        resets the activation key, while failing to activate within
+        the permitted window forbids later activation.
+        
+        """
+        valid_user = self.backend.register({}, 'alice', 'swordfish', 'alice@example.com')
+        valid_profile = RegistrationProfile.objects.get(user=valid_user)
+        activated = self.backend.activate({}, valid_profile.activation_key)
+        self.assertEqual(activated.username, valid_user.username)
+        self.failUnless(activated.is_active)
