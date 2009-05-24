d1e6c71ad3ec20b9bd17331d6d0be7d482ed0ec5
James Bennett <james@b-list.org>
2009-05-24 00:21:15 -0500

    Test email sending.

--- registration/tests.py
+++ registration/tests.py
@@ -3,6 +3,7 @@ Unit tests for django-registration.
 
 """
 
+from django.core import mail
 from django.test import TestCase
 
 
@@ -22,7 +23,8 @@ class DefaultBackendTestCase(TestCase):
     def test_registration(self):
         """
         Create a new user, verifying that username, email and password
-        are set correctly and that the new user is inactive.
+        are set correctly and that the new user is inactive and
+        received an activation email.
         
         """
         new_user = self.backend.register({}, 'bob', 'secret', 'bob@example.com')
@@ -30,3 +32,4 @@ class DefaultBackendTestCase(TestCase):
         self.failUnless(new_user.check_password('secret'))
         self.assertEqual(new_user.email, 'bob@example.com')
         self.failIf(new_user.is_active)
+        self.assertEqual(len(mail.outbox), 1)
