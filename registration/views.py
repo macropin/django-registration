10f52b79ec811721752fe659b94b4118531687ac
James Bennett <james@b-list.org>
2009-06-29 07:18:25 -0500

    Views should optionally accept a backend instance as an argument.

--- registration/views.py
+++ registration/views.py
@@ -14,20 +14,26 @@ from registration import get_backend
 
 def activate(request,
              template_name='registration/activate.html',
-             extra_context=None,
+             extra_context=None, backend=get_backend(),
              **kwargs):
     """
     Activate a user's account.
 
     The actual activation of the account will be delegated to the
-    backend specified by the ``REGISTRATION_BACKEND`` setting; the
+    backend specified by the ``REGISTRATION_BACKEND`` setting
+    (although this can be overridden; see argument list below); the
     backend's ``activate()`` method will be called, passing any
     keyword arguments captured from the URL, and will be assumed to
     return a ``User`` if activation was successful, or a value which
     evaluates to ``False`` in boolean context if not.
 
     **Optional arguments**
-       
+
+    ``backend``
+        An instance of a registration backend. If specified, will be
+        used in place of the backend specified by the
+        ``REGISTRATION_BACKEND`` setting.
+
     ``extra_context``
         A dictionary of variables to add to the template context. Any
         callable object in this dictionary will be called to produce
@@ -52,7 +58,6 @@ def activate(request,
     registration/activate.html or ``template_name`` keyword argument.
     
     """
-    backend = get_backend()
     account = backend.activate(request, **kwargs)
     if extra_context is None:
         extra_context = {}
@@ -67,13 +72,14 @@ def activate(request,
 def register(request, success_url=None, form_class=None,
              disallowed_url='registration_disallowed',
              template_name='registration/registration_form.html',
-             extra_context=None):
+             extra_context=None, backend=get_backend()):
     """
     Allow a new user to register an account.
 
     The actual registration of the account will be delegated to the
-    backend specified by the ``REGISTRATION_BACKEND`` setting; it will
-    be used as follows:
+    backend specified by the ``REGISTRATION_BACKEND`` setting
+    (although this can be overridden; see argument list below); it
+    will be used as follows:
 
     1. The backend's ``registration_allowed()`` method will be called,
        passing the ``HttpRequest``, to determine whether registration
@@ -103,6 +109,11 @@ def register(request, success_url=None, form_class=None,
     None.
     
     **Optional arguments**
+
+    ``backend``
+        An instance of a registration backend. If specified, will be
+        used in place of the backend specified by the
+        ``REGISTRATION_BACKEND`` setting.
 
     ``disallowed_url``
         URL to redirect to if registration is not permitted for the
@@ -144,7 +155,6 @@ def register(request, success_url=None, form_class=None,
     argument.
     
     """
-    backend = get_backend()
     if not backend.registration_allowed(request):
         return redirect(disallowed_url)
     if form_class is None:
