bd5d9920115088609712bf614057364153fc8d3e
James Bennett <james@b-list.org>
2009-09-16 03:09:07 -0500

    Factor out the auth URLs into their own URLConf for easier including/reusability.

--- registration/backends/default/urls.py
+++ registration/backends/default/urls.py
@@ -6,12 +6,12 @@ If the default behavior of these views is acceptable to you, simply
 use a line like this in your root URLConf to set up the default URLs
 for registration::
 
-    (r'^accounts/', include('registration.backend.default.urls')),
+    (r'^accounts/', include('registration.backends.default.urls')),
 
 This will also automatically set up the views in
 ``django.contrib.auth`` at sensible default locations.
 
-But if you'd like to customize the behavior (e.g., by passing extra
+If you'd like to customize the behavior (e.g., by passing extra
 arguments to the various views) or split up the URLs, feel free to set
 up your own URL patterns for these views instead.
 
@@ -20,7 +20,6 @@ up your own URL patterns for these views instead.
 
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
-from django.contrib.auth import views as auth_views
 
 from registration.views import activate
 from registration.views import register
@@ -35,32 +34,6 @@ urlpatterns = patterns('',
                            activate,
                            { 'backend': 'registration.backends.default.DefaultBackend' },
                            name='registration_activate'),
-                       url(r'^login/$',
-                           auth_views.login,
-                           {'template_name': 'registration/login.html'},
-                           name='auth_login'),
-                       url(r'^logout/$',
-                           auth_views.logout,
-                           {'template_name': 'registration/logout.html'},
-                           name='auth_logout'),
-                       url(r'^password/change/$',
-                           auth_views.password_change,
-                           name='auth_password_change'),
-                       url(r'^password/change/done/$',
-                           auth_views.password_change_done,
-                           name='auth_password_change_done'),
-                       url(r'^password/reset/$',
-                           auth_views.password_reset,
-                           name='auth_password_reset'),
-                       url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-                           auth_views.password_reset_confirm,
-                           name='auth_password_reset_confirm'),
-                       url(r'^password/reset/complete/$',
-                           auth_views.password_reset_complete,
-                           name='auth_password_reset_complete'),
-                       url(r'^password/reset/done/$',
-                           auth_views.password_reset_done,
-                           name='auth_password_reset_done'),
                        url(r'^register/$',
                            register,
                            { 'backend': 'registration.backends.default.DefaultBackend' },
@@ -73,4 +46,5 @@ urlpatterns = patterns('',
                            direct_to_template,
                            { 'template': 'registration/registration_closed.html' },
                            name='registration_disallowed'),
+                       (r'', include('registration.auth_urls')),
                        )
