b362e6060abe631f25e5227664df4e1670f4d630
James Bennett <james@b-list.org>
2009-10-07 03:09:54 -0500

    Use raw_id_fields for the relation from RegistrationProfile to User, for sites which have huge numbers of users.

--- registration/admin.py
+++ registration/admin.py
@@ -5,6 +5,7 @@ from registration.models import RegistrationProfile
 
 class RegistrationAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'activation_key_expired')
+    raw_id_fields = ['user']
     search_fields = ('user__username', 'user__first_name')
 
 
