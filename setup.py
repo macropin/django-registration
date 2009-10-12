6bf4552cfef2cee78d9dcc494c75662a72df0a64
James Bennett <james@b-list.org>
2009-10-12 02:49:45 -0500

    Update setup.py with impending version bump and beta status.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ for dirpath, dirnames, filenames in os.walk('registration'):
 
 
 setup(name='django-registration',
-      version='0.7',
+      version='0.8',
       description='An extensible user-registration application for Django',
       author='James Bennett',
       author_email='james@b-list.org',
@@ -33,7 +33,7 @@ setup(name='django-registration',
       package_dir={'registration': 'registration'},
       packages=packages,
       package_data={'registration': data_files},
-      classifiers=['Development Status :: 5 - Production/Stable',
+      classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Framework :: Django',
                    'Intended Audience :: Developers',
