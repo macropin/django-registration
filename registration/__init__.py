53bed4837805fa304153622689abb7c4c581ec73
James Bennett <james@b-list.org>
2013-03-20 17:46:47 -0700

    Move import of Django's get_version into django-registration's get_version, to avoid dependency-order problems.

--- registration/__init__.py
+++ registration/__init__.py
@@ -1,8 +1,6 @@
-from django.utils.version import get_version as django_get_version
-
-
 VERSION = (0, 9, 0, 'beta', 1)
 
 
 def get_version():
+    from django.utils.version import get_version as django_get_version
     return django_get_version(VERSION) # pragma: no cover
