6b81200d25094c9048f364ef7d6e30313b04befd
Andrew Cutler <macropin@gmail.com>
2014-08-15 22:18:46 +1000

    Need to specify MIDDLEWARE_CLASSES for Django 1.7 as defaults are too bare

--- test_app/test_settings.py
+++ test_app/test_settings.py
@@ -22,3 +22,11 @@ ROOT_URLCONF = 'test_app.urls'
 TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
 )
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
