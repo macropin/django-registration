ff1db1a285a09e4c9bb18a44500b4d029ebe474a
Andrew Cutler <macropin@gmail.com>
2014-08-15 20:40:28 +1000

    Django-1.7 and Python 3 support Pull request #2

--- registration/backends/default/views.py
+++ registration/backends/default/views.py
@@ -45,7 +45,7 @@ class RegistrationView(BaseRegistrationView):
     an instance of ``registration.models.RegistrationProfile``. See
     that model and its custom manager for full documentation of its
     fields and supported operations.
-    
+
     """
     def register(self, request, **cleaned_data):
         """
@@ -94,7 +94,7 @@ class RegistrationView(BaseRegistrationView):
 
         * If ``REGISTRATION_OPEN`` is both specified and set to
           ``False``, registration is not permitted.
-        
+
         """
         return getattr(settings, 'REGISTRATION_OPEN', True)
 
@@ -102,7 +102,7 @@ class RegistrationView(BaseRegistrationView):
         """
         Return the name of the URL to redirect to after successful
         user registration.
-        
+
         """
         return ('registration_complete', (), {})
 
@@ -117,7 +117,7 @@ class ActivationView(BaseActivationView):
         ``registration.signals.user_activated`` will be sent, with the
         newly activated ``User`` as the keyword argument ``user`` and
         the class of this backend as the sender.
-        
+
         """
         activated_user = RegistrationProfile.objects.activate_user(activation_key)
         if activated_user:
--- registration/tests/forms.py
+++ registration/tests/forms.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.test import TestCase
 
 from registration import forms
@@ -25,19 +27,19 @@ class RegistrationFormTests(TestCase):
                       'email': 'foo@example.com',
                       'password1': 'foo',
                       'password2': 'foo'},
-            'error': ('username', [u"This value may contain only letters, numbers and @/./+/-/_ characters."])},
+            'error': ('username', ["This value may contain only letters, numbers and @/./+/-/_ characters."])},
             # Already-existing username.
             {'data': {'username': 'alice',
                       'email': 'alice@example.com',
                       'password1': 'secret',
                       'password2': 'secret'},
-            'error': ('username', [u"A user with that username already exists."])},
+            'error': ('username', ["A user with that username already exists."])},
             # Mismatched passwords.
             {'data': {'username': 'foo',
                       'email': 'foo@example.com',
                       'password1': 'foo',
                       'password2': 'bar'},
-            'error': ('__all__', [u"The two password fields didn't match."])},
+            'error': ('__all__', ["The two password fields didn't match."])},
             ]
 
         for invalid_dict in invalid_data_dicts:
@@ -64,7 +66,7 @@ class RegistrationFormTests(TestCase):
                                                           'password2': 'foo'})
         self.failIf(form.is_valid())
         self.assertEqual(form.errors['tos'],
-                         [u"You must agree to the terms to register"])
+                         ["You must agree to the terms to register"])
 
         form = forms.RegistrationFormTermsOfService(data={'username': 'foo',
                                                           'email': 'foo@example.com',
@@ -89,7 +91,7 @@ class RegistrationFormTests(TestCase):
                                                        'password2': 'foo'})
         self.failIf(form.is_valid())
         self.assertEqual(form.errors['email'],
-                         [u"This email address is already in use. Please supply a different email address."])
+                         ["This email address is already in use. Please supply a different email address."])
 
         form = forms.RegistrationFormUniqueEmail(data={'username': 'foo',
                                                        'email': 'foo@example.com',
@@ -108,11 +110,11 @@ class RegistrationFormTests(TestCase):
                      'password2': 'foo'}
         for domain in forms.RegistrationFormNoFreeEmail.bad_domains:
             invalid_data = base_data.copy()
-            invalid_data['email'] = u"foo@%s" % domain
+            invalid_data['email'] = "foo@%s" % domain
             form = forms.RegistrationFormNoFreeEmail(data=invalid_data)
             self.failIf(form.is_valid())
             self.assertEqual(form.errors['email'],
-                             [u"Registration using free email addresses is prohibited. Please supply a different email address."])
+                             ["Registration using free email addresses is prohibited. Please supply a different email address."])
 
         base_data['email'] = 'foo@example.com'
         form = forms.RegistrationFormNoFreeEmail(data=base_data)
