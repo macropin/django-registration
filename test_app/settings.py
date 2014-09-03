f80e42179a5e340ec4006d8e50ec7f117dcb7b30
Andrew Cutler <macropin@gmail.com>
2014-09-03 16:28:47 +1000

    Add default settings

---
+++ test_app/settings.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'dr.sqlite3',
+    },
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.sites',
+    'django.contrib.sessions',
+    'django.contrib.contenttypes',
+    'registration',
+    'test_app',
+)
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+SECRET_KEY = '_'
+SITE_ID = 1
+ROOT_URLCONF = 'test_app.urls'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
