d9bcf36d4c8dd674c5495a735f9e3f5901751683
Andrew Cutler <macropin@gmail.com>
2014-09-03 16:38:13 +1000

    Fix test_app urls. Still doesn't do anything useful

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,10 +1,8 @@
-# coding: utf-8
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-from registration.views import LoginView
 
 urlpatterns = patterns('',
     (r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^login/', 'django.contrib.auth.views.login', name='login'),
     url(r'^accounts/profile/', TemplateView.as_view(template_name='registration/registration_complete.html'), name='profile'),
 )
