623baa742724ed915fe93450b39898c543cd930c
Andrew Cutler <macropin@gmail.com>
2014-10-01 12:15:14 +1000

    only bind signal if REGISTRATION_AUTO_LOGIN True

--- registration/signals.py
+++ registration/signals.py
@@ -12,10 +12,11 @@ user_activated = Signal(providing_args=["user", "request"])
 
 def login_user(sender, user, request, **kwargs):
     """ Automatically authenticate the user when activated  """
-    if getattr(settings, 'REGISTRATION_AUTO_LOGIN', False ):
-        backend = get_backends()[0]  # Hack to bypass `authenticate()`.
-        user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
-        login(request, user)
-        request.session['REGISTRATION_AUTO_LOGIN'] = True
-        request.session.modified = True
-user_activated.connect(login_user)
+    backend = get_backends()[0]  # Hack to bypass `authenticate()`.
+    user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
+    login(request, user)
+    request.session['REGISTRATION_AUTO_LOGIN'] = True
+    request.session.modified = True
+
+if getattr(settings, 'REGISTRATION_AUTO_LOGIN', False):
+    user_activated.connect(login_user)
