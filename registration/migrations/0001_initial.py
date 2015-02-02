a034e1a8b3d81d43e124274f177b938900905c60
Tim Graham <timograham@gmail.com>
2015-02-02 14:50:06 -0500

    Add initial migration for Django 1.7.

---
+++ registration/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='RegistrationProfile',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('activation_key', models.CharField(verbose_name='activation key', max_length=40)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL, verbose_name='user')),
+            ],
+            options={
+                'verbose_name': 'registration profile',
+                'verbose_name_plural': 'registration profiles',
+            },
+            bases=(models.Model,),
+        ),
+    ]
