72ed4df5303b339c18cd766f72f988d0a2111b70
Tim Graham <timograham@gmail.com>
2016-06-14 14:33:53 -0400

    Fixed #190: Drop Django 1.7 support.

--- docs/release-notes.rst
+++ docs/release-notes.rst
@@ -4,4 +4,4 @@ Release notes
 =============
 
 The |version| release of |project| supports Python 2.7, 3.4, 3.5 and
-Django 1.7, 1.8, and 1.9.
+Django 1.8 and 1.9.
--- registration/migrations/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Django migrations for django-registration-redux
-
-This package does not contain South migrations.
-
-These are Django native migrations. They require Django > 1.7.
-
-"""
