4fe7970e67d7078c524bc8c076e77ae925ee52fb
Sergios Aftsidis <safts@noc.grnet.gr>
2016-06-29 15:37:47 +0300

    Change order of deletion in ``RegistrationManager.delete_expired_users()`` to inherit it
    Executing user.delete() first and then profile.delete() is a logical error. It was not visible
    before but now it is. ``RegistrationProfile`` has a ForeignKey (actually a OneToOneField
    which cascades delete) to the user, so when the user was deleted his related ``RegistrationProfile``
    was deleted too and when ``SupervisedRegistrationProfile`` was deleted it yielded the following error:
    
    self.model.MultipleObjectsReturned( DoesNotExist: RegistrationProfile matching query does not exist)

--- registration/models.py
+++ registration/models.py
@@ -240,8 +240,8 @@ class RegistrationManager(models.Manager):
                 if profile.activation_key_expired():
                     user = profile.user
                     if not user.is_active:
-                        user.delete()
                         profile.delete()
+                        user.delete()
             except UserModel().DoesNotExist:
                 profile.delete()
 
@@ -554,56 +554,6 @@ class SupervisedRegistrationManager(RegistrationManager):
             admins, ctx_dict, admin_approve_email_subject,
             admin_approve_email_body, admin_approve_email_html
         )
-
-    def delete_expired_users(self):
-        """
-        Remove expired instances of ``SupervisedRegistrationProfile`` and their
-        associated ``User``s.
-
-        Accounts to be deleted are identified by searching for
-        instances of ``RegistrationProfile`` with expired activation
-        keys, and then checking to see if their associated ``User``
-        instances have the field ``is_active`` set to ``False``; any
-        ``User`` who is both inactive and has an expired activation
-        key will be deleted.
-
-        It is recommended that this method be executed regularly as
-        part of your routine site maintenance; this application
-        provides a custom management command which will call this
-        method, accessible as ``manage.py cleanupregistration``.
-
-        Regularly clearing out accounts which have never been
-        activated serves two useful purposes:
-
-        1. It alleviates the ocasional need to reset a
-           ``SupervisedRegistrationProfile`` and/or re-send an activation email
-           when a user does not receive or does not act upon the
-           initial activation email; since the account will be
-           deleted, the user will be able to simply re-register and
-           receive a new activation key.
-
-        2. It prevents the possibility of a malicious user registering
-           one or more accounts and never activating them (thus
-           denying the use of those usernames to anyone else); since
-           those accounts will be deleted, the usernames will become
-           available for use again.
-
-        If you have a troublesome ``User`` and wish to disable their
-        account while keeping it in the database, simply delete the
-        associated ``SupervisedRegistrationProfile``; an inactive ``User`` which
-        does not have an associated ``RegistrationProfile`` will not
-        be deleted.
-
-        """
-        for profile in self.all():
-            try:
-                if profile.activation_key_expired():
-                    user = profile.user
-                    if not user.is_active:
-                        profile.delete()
-                        user.delete()
-            except UserModel().DoesNotExist:
-                profile.delete()
 
 
 class SupervisedRegistrationProfile(RegistrationProfile):
