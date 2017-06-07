741c935e3486c5d1efa8bbf06db49ebaf57658fa
Joshua Blum <jblum18@gmail.com>
2017-06-07 17:54:29 -0400

    Prevent leaking password reset token through Referrer header

--- registration/auth_urls.py
+++ registration/auth_urls.py
@@ -27,35 +27,54 @@ from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import views as auth_views
 
+# Attempt to use the auth class based views if available.
+try:
+    login_view = auth_views.LoginView.as_view()
+    logout_view = auth_views.LogoutView.as_view()
+    password_change_view = auth_views.PasswordChangeView.as_view()
+    password_change_done_view = auth_views.PasswordChangeDoneView.as_view()
+    password_reset_view = auth_views.PasswordResetView.as_view()
+    password_reset_complete_view = auth_views.PasswordResetCompleteView.as_view()
+    password_reset_done_view = auth_views.PasswordResetDoneView.as_view()
+    password_reset_confirm_view = auth_views.PasswordResetConfirmView.as_view()
+except AttributeError:
+    login_view = auth_views.login
+    logout_view = auth_views.logout
+    password_change_view = auth_views.password_change
+    password_change_done_view = auth_views.password_change_done
+    password_reset_view = auth_views.password_reset
+    password_reset_complete_view = auth_views.password_reset_complete
+    password_reset_done_view = auth_views.password_reset_done
+    password_reset_confirm_view = auth_views.password_reset_confirm
 
 urlpatterns = [
     url(r'^login/$',
-        auth_views.login,
+        login_view,
         {'template_name': 'registration/login.html'},
         name='auth_login'),
     url(r'^logout/$',
-        auth_views.logout,
+        logout_view,
         {'template_name': 'registration/logout.html'},
         name='auth_logout'),
     url(r'^password/change/$',
-        auth_views.password_change,
+        password_change_view,
         {'post_change_redirect': reverse_lazy('auth_password_change_done')},
         name='auth_password_change'),
     url(r'^password/change/done/$',
-        auth_views.password_change_done,
+        password_change_done_view,
         name='auth_password_change_done'),
     url(r'^password/reset/$',
-        auth_views.password_reset,
+        password_reset_view,
         {'post_reset_redirect': reverse_lazy('auth_password_reset_done')},
         name='auth_password_reset'),
     url(r'^password/reset/complete/$',
-        auth_views.password_reset_complete,
+        password_reset_complete_view,
         name='auth_password_reset_complete'),
     url(r'^password/reset/done/$',
-        auth_views.password_reset_done,
+        password_reset_done_view,
         name='auth_password_reset_done'),
     url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
-        auth_views.password_reset_confirm,
+        password_reset_confirm_view,
         {'post_reset_redirect': reverse_lazy('auth_password_reset_complete')},
         name='auth_password_reset_confirm'),
 ]
