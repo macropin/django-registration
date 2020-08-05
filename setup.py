eb2475bdbf836faea7f05b6c77e09d2a06608361
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2020-08-05 20:37:13 +0900

    Add Django3.1 to tox

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
