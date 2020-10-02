9bfaeae5fdc90196cc11a0405e5e7d3fe0d35077
sujit2008 <65438579+sujit2008@users.noreply.github.com>
2020-10-02 10:45:59 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,5 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_app.settings")
-
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
