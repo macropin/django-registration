4a4f90ba1af332f66d44f6d18c3b14698dcd1b88
Evan Zhang <me@evanzhang.ca>
2023-10-11 22:40:59 +0100

    fix verbose name (#440)

--- registration/apps.py
+++ registration/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class RegistrationConfig(AppConfig):
     name = 'registration'
-    verbose_name = "Django-registration provides user registration functionality for Django websites."
+    verbose_name = "Registration"
     default_auto_field = 'django.db.models.AutoField'
